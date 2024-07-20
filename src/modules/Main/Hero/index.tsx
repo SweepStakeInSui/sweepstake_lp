8209becaa1bd3aa1830bdd993a0bdf3e144e7def
croissain <nhattho03@gmail.com>
2024-07-20 11:04:25 +0700

    :feature: update hero page

--- src/modules/Main/Hero/index.tsx
+++ src/modules/Main/Hero/index.tsx
@@ -11,38 +11,44 @@ import Container from '@/layouts/Container';
 import s from './style.module.scss';
 
 export default function MainHero() {
+  const decorRef = useRef<HTMLDivElement>(null);
   const titleRef = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLDivElement>(null);
   const globeRef = useRef<HTMLDivElement>(null);
 
   useGSAP(
     () => {
+      if (decorRef.current)
+        gsap.to(decorRef.current, {
+          opacity: 1,
+          delay: 1.6,
+          duration: 1,
+        });
+
       if (globeRef.current)
         gsap.to(globeRef.current, {
           scale: 1,
           opacity: 1,
           delay: 1.7,
           duration: 1.5,
+          y: '-5%',
         });
 
       if (buttonRef.current) {
         gsap.to(buttonRef.current, {
           scale: 1,
-          opacity: 1,
-          filter: 'blur(0px)',
-          delay: 1.3,
-          duration: 1,
+          delay: 2.5,
+          duration: 0.5,
+          clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
         });
       }
 
       if (titleRef.current) {
         gsap.to(titleRef.current, {
           scale: 1,
-          opacity: 1,
-          y: 0,
-          filter: 'blur(0px)',
-          delay: 1,
+          delay: 1.5,
           duration: 1,
+          clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
         });
       }
     },
@@ -51,8 +57,9 @@ export default function MainHero() {
 
   return (
     <Container className={s.hero}>
+      <div className={s.hero_decor} ref={decorRef} />
       <div className={s.hero_title_wrapper}>
-        <div ref={titleRef} className={s.hero_title_inner}>
+        <div className={s.hero_title_inner} ref={titleRef}>
           <Typography tag="h1" variant="display2" className={s.hero_title}>
             The next-gen prediction market is coming
           </Typography>
